16077dce359e30b22dc2b3b740221e604244b665
KyleSuttie <suttiebuddy02@aol.com>
2010-06-18 10:01:04 -0400

    JUST ADDED LANGUAGE SWITCH, ABOUT TO START BUILT IN TESTING

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'bd2e89f8100af2dc7efea4fe50165041cb2eea6355561a5f8d1294ed1ad84c28f408d43f995d08f4cd52804cfc745d300134b00c9349d1eee25c42495dc8e630';
