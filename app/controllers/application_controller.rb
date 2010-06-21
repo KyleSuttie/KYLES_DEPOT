19786c11c7bf06c1d6cd436dd8ae65ce50015363
KyleSuttie <suttiebuddy02@aol.com>
2010-06-21 12:08:18 -0400

    DEPOT APP FINISHED, JUST ADDED BUILT IN TESTING

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => '8fc080370e56e929a2d5afca5540a0f7'
+  protect_from_forgery #:secret => '8fc080370e56e929a2d5afca5540a0f7'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
